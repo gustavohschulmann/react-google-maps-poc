67de734cac4d66b315e3e0498b46814ddb2b9569
gustavoschulmann <gustavohschulmann@gmail.com>
2022-06-10 14:19:38 -0300

    onkeydown enter

--- src/components/ui/SearchBar/index.js
+++ src/components/ui/SearchBar/index.js
@@ -3,15 +3,19 @@ import React, { useState } from "react";
 import { StyledInput, StyledX } from "./styles";
 import { MagnifyingGlass } from "phosphor-react";
 
-const SearchBar = ({ onKeyDown }) => {
+const SearchBar = () => {
     const [value, setValue] = useState("");
+
+    const handleKeyDown = (event) => {
+        if (event.keyCode == 13) setValue("");
+    };
 
     return (
         <StyledInput
             value={value}
             onChange={(e) => setValue(e.target.value)}
+            onKeyDown={handleKeyDown}
             placeholder="110 Yonge St. Toronto"
-            onKeyDown={onKeyDown}
             prefix={<MagnifyingGlass />}
             suffix={<StyledX onClick={() => setValue("")} />}
         />
